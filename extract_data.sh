35675734fd11f0ecbb7be4a995670adeabe92fbb
noway <noway@011a-debian.jinr.ru>
2016-02-23 13:33:46 +0300

    Remove unnecessary files

--- extract_data.sh
+++
@@ -1,1 +0,0 @@
-awk '/### Grid/ {flag=1; for (i = 1; i <= 4; i++) getline;} /### Particles/{flag=0} flag {print}' $@
--- particles.sh
+++
@@ -1,1 +0,0 @@
-awk '/### Particles/ {flag=1; for (i = 1; i <= 2; i++) getline;} flag {print}' $@
