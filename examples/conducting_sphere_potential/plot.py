28939b43dc72c0c1e8223691a80e2518833f277c
noway <noway@011a-debian.jinr.ru>
2016-04-26 13:13:46 +0300

    Better plotting scripts in examples.

--- examples/conducting_sphere_potential/plot.py
+++ examples/conducting_sphere_potential/plot.py
@@ -2,6 +2,62 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def main():
+    outfile_name = "conducting_sphere_potential_fieldsWithoutParticles.h5"
+    extract_sphere_properties_from_out_file( outfile_name )
+    node_coords, phi = extract_full_nodecoords_and_potential_from_out_file( outfile_name )
+    num_subset = num_subset_along_z( node_coords, phi )
+    an = an_potential_at_num_points( num_subset )
+    plot_comparision( num_subset, an )
+
+    
+def extract_sphere_properties_from_out_file( outfile_name ):
+    outfile = h5py.File( outfile_name, driver="core", mode="r")
+    sphere_object_hdf5_group = outfile['/Inner_regions/sphere_object']
+    global sphere_center_x, sphere_center_y, sphere_center_z
+    global sphere_radius, sphere_phi
+    sphere_center_x = sphere_object_hdf5_group.attrs['origin_x']
+    sphere_center_y = sphere_object_hdf5_group.attrs['origin_y']
+    sphere_center_z = sphere_object_hdf5_group.attrs['origin_z']
+    sphere_radius = sphere_object_hdf5_group.attrs['radius']
+    sphere_phi = sphere_object_hdf5_group.attrs['potential']
+    outfile.close()
+
+    
+def extract_full_nodecoords_and_potential_from_out_file( outfile_name ):
+    outfile = h5py.File( outfile_name, driver="core", mode="r" )
+    node_coords_hdf5 = outfile['/Spatial_mesh/node_coordinates']
+    num_potential_hdf5 = outfile['/Spatial_mesh/potential']
+    node_coords = np.empty_like( node_coords_hdf5 )
+    node_coords_hdf5.read_direct( node_coords )
+    num_potential = np.empty_like( num_potential_hdf5 )
+    num_potential_hdf5.read_direct( num_potential )
+    outfile.close()
+    return( node_coords, num_potential )
+
+
+def num_subset_along_z( node_coords, phi ):
+    num_subset = []
+    for ( (x,y,z), p ) in zip(node_coords, phi):
+        if ( x == sphere_center_x and y == sphere_center_y ):
+            num_subset.append( (x, y, z, p) )            
+    num_subset = np.array( num_subset,
+                           dtype=[('x', 'float'), ('y', 'float'),
+                                  ('z', 'float'), ('phi', 'float')] )
+    return( num_subset )
+    
+
+def an_potential_at_num_points( num_subset ):
+    an = np.empty_like( num_subset )
+    for ( i, (x, y, z) ) in enumerate( num_subset[['x','y','z']] ):
+        phi = spherical_potential( x, y, z,
+                                   sphere_center_x, sphere_center_y, sphere_center_z,
+                                   sphere_radius, sphere_phi )
+        an[i] = ( x, y, z, phi )
+    return an
+
+
 def spherical_potential( x, y, z,
                          center_x, center_y, center_z,
                          radius, phi_on_sphere ):
@@ -14,59 +70,17 @@ def spherical_potential( x, y, z,
         potential = phi_on_sphere * radius / dist
     return( potential )
 
-calculation_results = h5py.File( 
-    "conducting_sphere_potential_fieldsWithoutParticles.h5", 
-    driver="core", 
-    mode="r")
 
-node_coords_hdf5 = calculation_results['/Spatial_mesh/node_coordinates']
-node_coords = np.empty_like( node_coords_hdf5 )
-node_coords_hdf5.read_direct( node_coords )
+def plot_comparision( num, an ):    
+    plt.plot( num['z'], num['phi'], 
+              linestyle='', marker='o',
+              label = "Num" )
+    plt.plot( an['z'], an['phi'],
+              label = "An" )
+    plt.legend()
+    print( "Saving potential comparison to 'potential_along_z.png'")
+    plt.savefig('potential_along_z.png')
 
-num_potential_hdf5 = calculation_results['/Spatial_mesh/potential']
-num_potential = np.empty_like( num_potential_hdf5 )
-num_potential_hdf5.read_direct( num_potential )
-
-sphere_object_hdf5_group = calculation_results['/Inner_regions/sphere_object']
-sphere_center_x = sphere_object_hdf5_group.attrs['origin_x']
-sphere_center_y = sphere_object_hdf5_group.attrs['origin_y']
-sphere_center_z = sphere_object_hdf5_group.attrs['origin_z']
-sphere_radius = sphere_object_hdf5_group.attrs['radius']
-sphere_phi = sphere_object_hdf5_group.attrs['potential']
-current_sphere_potential = lambda x,y,z: spherical_potential( x, y, z,
-                                                              sphere_center_x,
-                                                              sphere_center_y,
-                                                              sphere_center_z,
-                                                              sphere_radius,
-                                                              sphere_phi )
-                                                              
+main()
 
 
-### Subset points.
-line_along_z_idx = np.empty( len(node_coords), dtype = int )
-current = 0
-for idx, (x, y, z) in enumerate( node_coords ):
-    if ( x == 5.0 and y == 5.0 ):
-        line_along_z_idx[current] = idx
-        current = current + 1
-line_along_z_idx = np.resize( line_along_z_idx, current )
-
-
-z_coords_subset = np.empty( len(line_along_z_idx) )
-for i, idx in enumerate( line_along_z_idx ):
-    z_coords_subset[i] = node_coords[idx][2]
-num_potential_subset = num_potential[ line_along_z_idx ]
-analit_potential_subset = np.empty( len( line_along_z_idx ) )
-for i, idx in enumerate( line_along_z_idx ):
-    x, y, z = node_coords[idx]
-    analit_potential_subset[i] = current_sphere_potential( x, y, z ) 
-
-plt.plot( z_coords_subset, num_potential_subset, 
-    linestyle='', marker='o',
-    label = "Num" )
-plt.plot( z_coords_subset, analit_potential_subset,
-    label = "An" )
-plt.legend()
-plt.savefig('potential_along_z.png')
-#plot( line_along_z$vec_z, line_along_z$phi )
-#lines( line_along_z$vec_z, line_along_z$an, add=T )
---
+++ examples/single_particle_in_magnetic_field/run_example.sh
@@ -0,0 +1,2 @@
+../../epicf.out single_particle_in_magnetic_field.conf
+python3 plot.py
