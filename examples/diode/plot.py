9b8bb393c84cd04c8933fee04faa75d440a63193
noway <noway@011a-debian.jinr.ru>
2016-04-28 12:49:02 +0300

    WIP: diode example
    Read computation volume paramters from out-file.
    Attempt to draw electric field.

--- examples/diode/plot.py
+++ examples/diode/plot.py
@@ -5,46 +5,62 @@ import matplotlib.pyplot as plt
 
 def main():
     outfile_name = "diode_fieldsWithoutParticles.h5"
-    #extract_boundary_box_properties_from_out_file( outfile_name )
-    node_coords, phi = extract_full_nodecoords_and_potential_from_out_file( outfile_name )
-    num_subset = num_subset_middle_plane( node_coords, phi )
+    extract_boundary_box_properties_from_out_file( outfile_name )
+    node_coords, phi, el_field = extract_full_nodecoords_potential_fields_from_out_file( outfile_name )
+    num_subset = num_subset_middle_plane( node_coords, phi, el_field )
     plot_potential( num_subset )
+    plot_fields( num_subset )
 
     
 def extract_boundary_box_properties_from_out_file( outfile_name ):
-    print( "nothing" )
-
+    outfile = h5py.File( outfile_name, driver="core", mode="r" )
+    global x_cell_size, x_n_nodes, x_volume_size
+    global y_cell_size, y_n_nodes, y_volume_size
+    global z_cell_size, z_n_nodes, z_volume_size
+    x_cell_size = outfile["/Spatial_mesh"].attrs["x_cell_size"][0]
+    y_cell_size = outfile["/Spatial_mesh"].attrs["y_cell_size"][0]
+    z_cell_size = outfile["/Spatial_mesh"].attrs["z_cell_size"][0]
+    x_n_nodes = outfile["/Spatial_mesh"].attrs["x_n_nodes"][0]
+    y_n_nodes = outfile["/Spatial_mesh"].attrs["y_n_nodes"][0]
+    z_n_nodes = outfile["/Spatial_mesh"].attrs["z_n_nodes"][0]
+    x_volume_size = outfile["/Spatial_mesh"].attrs["x_volume_size"][0]
+    y_volume_size = outfile["/Spatial_mesh"].attrs["y_volume_size"][0]
+    z_volume_size = outfile["/Spatial_mesh"].attrs["z_volume_size"][0]    
+    outfile.close()
     
-def extract_full_nodecoords_and_potential_from_out_file( outfile_name ):
+def extract_full_nodecoords_potential_fields_from_out_file( outfile_name ):
     outfile = h5py.File( outfile_name, driver="core", mode="r" )
     node_coords_hdf5 = outfile['/Spatial_mesh/node_coordinates']
     num_potential_hdf5 = outfile['/Spatial_mesh/potential']
+    el_field_hdf5 = outfile['/Spatial_mesh/electric_field']
     node_coords = np.empty_like( node_coords_hdf5 )
-    node_coords_hdf5.read_direct( node_coords )
     num_potential = np.empty_like( num_potential_hdf5 )
+    el_field = np.empty_like( el_field_hdf5 )
+    node_coords_hdf5.read_direct( node_coords )
     num_potential_hdf5.read_direct( num_potential )
+    el_field_hdf5.read_direct( el_field )
     outfile.close()
-    return( node_coords, num_potential )
+    return( node_coords, num_potential, el_field )
 
 
-def num_subset_middle_plane( node_coords, phi ):    
-    grid_y_size = 0.6 # todo: read from file
+def num_subset_middle_plane( node_coords, phi, el_field ):
+    plane_at_half_y = int( ( y_n_nodes - 1 ) / 2 ) * y_cell_size
     num_subset = []
-    for ( (x,y,z), p ) in zip(node_coords, phi):
-        if ( y == grid_y_size / 2 ):
-            num_subset.append( (x, y, z, p) )            
+    for ( (x,y,z), p, (ex,ey,ez) ) in zip(node_coords, phi, el_field):
+        if ( y == plane_at_half_y ):
+            num_subset.append( (x, y, z, p, ex, ey, ez) )            
     num_subset = np.array( num_subset,
-                           dtype=[('x', 'float'), ('y', 'float'),
-                                  ('z', 'float'), ('phi', 'float')] )
+                           dtype=[('x', 'float'), ('y', 'float'), ('z', 'float'),
+                                  ('phi', 'float'),
+                                  ('Ex', 'float'), ('Ey', 'float'), ('Ez', 'float') ] )
     return( num_subset )
     
 
 def plot_potential( num ):
     # todo: sort num before reshaping
-    # todo: get size from out-file
-    X = num['x'].reshape( ( 31,101 ) )
-    Y = num['z'].reshape( ( 31,101 ) )
-    Z = num['phi'].reshape( ( 31,101 ) )
+    X = num['x'].reshape( ( x_n_nodes, z_n_nodes ) )
+    Y = num['z'].reshape( ( x_n_nodes, z_n_nodes ) )
+    Z = num['phi'].reshape( ( x_n_nodes, z_n_nodes ) )
     plt.figure()
     levels = np.linspace( Z.min(), Z.max(), 20 )
     CS = plt.contourf( X, Y, Z, levels )
@@ -52,11 +68,50 @@ def plot_potential( num ):
     cbar.ax.set_ylabel('Potential, [CGS]')
     plt.xlabel( "X [cm]" )
     plt.ylabel( "Z [cm]" )
-    plt.title('Potential at Y = y_size / 2')
-    plt.show()    
-    #print( "Saving potential comparison to 'potential_along_z.png'")
-    #plt.savefig('potential_along_z.png')
+    plt.title('Potential at Y = y_size / 2 = {0}'.format( int( ( y_n_nodes - 1 ) / 2 ) * y_cell_size ) )
+    #plt.show()    
+    #print( "Saving potential plot to 'potential.png'")
+    plt.savefig('potential.png')
 
+def plot_fields( num ):
+    plot_fields_quiver( num )
+    plot_fields_streamplot( num )
+    
+def plot_fields_quiver( num ):
+    # todo: sort num before reshaping
+    X = num['x'].reshape( ( x_n_nodes, z_n_nodes ) )
+    Y = num['z'].reshape( ( x_n_nodes, z_n_nodes ) )
+    U = num['Ex'].reshape( ( x_n_nodes, z_n_nodes ) )
+    V = num['Ez'].reshape( ( x_n_nodes, z_n_nodes ) )
+    plt.figure()
+    Q = plt.quiver( X, Y, U, V,  )
+    qk = plt.quiverkey(Q, 0.9, 0.95, 5, 'Ex-Ez',
+                       labelpos='E',
+                       coordinates='figure',
+                       fontproperties={'weight': 'bold'})
+    #cbar.ax.set_ylabel('Potential, [CGS]')
+    plt.xlabel( "X [cm]" )
+    plt.ylabel( "Z [cm]" )
+    plt.title('Ex, Ez at Y = y_size / 2 = {0}'.format( int( ( y_n_nodes - 1 ) / 2 ) * y_cell_size ) )
+    #plt.show()    
+    #print( "Saving potential comparison to 'potential_along_z.png'")
+    plt.savefig('field_quiver.png')
+    
+def plot_fields_streamplot( num ):
+    # todo: sort num before reshaping
+    X = np.unique( num['x'] )
+    Y = np.unique( num['z'] )
+    U = np.transpose( num['Ex'].reshape( ( x_n_nodes, z_n_nodes ) ) )
+    V = np.transpose( num['Ez'].reshape( ( x_n_nodes, z_n_nodes ) ) )
+    plt.figure()
+    plt.streamplot(X, Y, U, V, color='r')
+    plt.xlabel( "X [cm]" )
+    plt.ylabel( "Z [cm]" )
+    plt.title('Ex, Ez at Y = y_size / 2 = {0}'.format( int( ( y_n_nodes - 1 ) / 2 ) * y_cell_size ) )
+    #plt.show()    
+    #print( "Saving potential comparison to 'potential_along_z.png'")
+    plt.savefig('field_streamplot.png')
+    
 main()
 
 
