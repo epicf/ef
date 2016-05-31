48baf9024b800446ec046523f24a0dcb1ec41113
noway <noway@011a-debian.jinr.ru>
2016-05-31 11:19:25 +0300

    WIP: cylindrical source
    Initial refactoring of box-source.

--- particle_source.h
+++ particle_source.h
@@ -5,7 +5,9 @@
 #include <random>
 #include <iostream>
 #include <iomanip>
+#include <string>
 #include <vector>
+#include <boost/ptr_container/ptr_vector.hpp>
 #include <hdf5.h>
 #include <hdf5_hl.h>
 #include <mpi.h>
@@ -16,18 +18,12 @@
 class Particle_source{
 public:
     std::string name;
+    std::string geometry_type;
     std::vector<Particle> particles;
 private:
     int initial_number_of_particles;
     int particles_to_generate_each_step;
     unsigned int max_id;
-    // Source position
-    double xleft;
-    double xright;
-    double ytop;
-    double ybottom;
-    double znear;
-    double zfar;
     // Momentum
     Vec3d mean_momentum;
     double temperature;
@@ -37,31 +33,66 @@ private:
     // Random number generator
     std::default_random_engine rnd_gen;
 public:
-    Particle_source( Config &conf, Source_config_part &src_conf );
-    void generate_each_step();
-    void update_particles_position( double dt );	
-    void print_particles();
-    void write_to_file( hid_t hdf5_file_id );
+    // virtual Particle_source( Config &conf, Source_config_part &src_conf );
+    virtual void generate_each_step();
+    virtual void update_particles_position( double dt );	
+    virtual void print_particles();
+    virtual void write_to_file( hid_t hdf5_file_id );
     virtual ~Particle_source() {};
 private:
     // Particle initialization
-    void set_parameters_from_config( Source_config_part &src_conf );
-    void generate_initial_particles();
+    virtual void set_parameters_from_config( Source_config_part &src_conf );
+    virtual void generate_initial_particles();
     // Todo: replace 'std::default_random_engine' type with something more general.
-    void generate_num_of_particles( int num_of_particles );
-    int num_of_particles_for_each_process( int num_of_particles );
-    void populate_vec_of_ids( std::vector<int> &vec_of_ids,
-			      int num_of_particles_for_this_proc );
+    virtual void generate_num_of_particles( int num_of_particles );
+    virtual int num_of_particles_for_each_process( int num_of_particles );
+    virtual void populate_vec_of_ids( std::vector<int> &vec_of_ids,
+				      int num_of_particles_for_this_proc );
     //int generate_particle_id( const int number, const int proc );
+    virtual double random_in_range( const double low, const double up,
+				    std::default_random_engine &rnd_gen );
+    virtual Vec3d maxwell_momentum_distr( const Vec3d mean_momentum,
+					  const double temperature, const double mass, 
+					  std::default_random_engine &rnd_gen );
+    // Check config
+    virtual void check_correctness_of_related_config_fields( 
+	Config &conf, Source_config_part &src_conf );
+    virtual void particle_source_initial_number_of_particles_gt_zero( 
+	Config &conf, Source_config_part &src_conf );
+    virtual void particle_source_particles_to_generate_each_step_ge_zero( 
+	Config &conf, Source_config_part &src_conf );
+    virtual void particle_source_temperature_gt_zero( 
+	Config &conf, Source_config_part &src_conf );
+    virtual void particle_source_mass_gt_zero( 
+	Config &conf, Source_config_part &src_conf );
+    // Write to file
+    void write_hdf5_particles( hid_t current_source_group_id );
+    void write_hdf5_source_parameters( hid_t current_source_group_id );
+    void hdf5_status_check( herr_t status );
+    int total_particles_across_all_processes();
+    int data_offset_for_each_process_for_1d_dataset();
+};
+
+
+class Particle_source_box : Particle_source {
+private:
+    // Source position
+    double xleft;
+    double xright;
+    double ytop;
+    double ybottom;
+    double znear;
+    double zfar;
+public:
+    Particle_source_box( Config &conf, Source_config_part &src_conf );
+    virtual ~Particle_source_box() {};
+private:
+    // Particle initialization
+    void set_parameters_from_config( Source_config_part &src_conf );
     Vec3d uniform_position_in_cube( const double xleft, const double ytop,
 				    const double znear, const double xright,
 				    const double ybottom, const double zfar,
 				    std::default_random_engine &rnd_gen );
-    double random_in_range( const double low, const double up,
-			    std::default_random_engine &rnd_gen );
-    Vec3d maxwell_momentum_distr( const Vec3d mean_momentum,
-				  const double temperature, const double mass, 
-				  std::default_random_engine &rnd_gen );
     // Check config
     void check_correctness_of_related_config_fields( 
 	Config &conf, Source_config_part &src_conf );
@@ -92,17 +123,13 @@ private:
     void particle_source_mass_gt_zero( 
 	Config &conf, Source_config_part &src_conf );
     // Write to file
-    void write_hdf5_particles( hid_t current_source_group_id );
     void write_hdf5_source_parameters( hid_t current_source_group_id );
-    void hdf5_status_check( herr_t status );
-    int total_particles_across_all_processes();
-    int data_offset_for_each_process_for_1d_dataset();
 };
 
 
 class Particle_sources_manager{
 public:
-    std::vector<Particle_source> sources;
+    boost::ptr_vector<Particle_source> sources;
 public:
     Particle_sources_manager( Config &conf );
     virtual ~Particle_sources_manager() {};
