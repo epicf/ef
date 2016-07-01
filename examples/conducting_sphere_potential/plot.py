13a181e467acceb4cfa464ff28e56c6f15417af1
noway <noway@011a-debian.jinr.ru>
2016-07-01 12:30:29 +0300

    Colormap potential in example with conducting sph

--- examples/conducting_sphere_potential/plot.py
+++ examples/conducting_sphere_potential/plot.py
@@ -7,9 +7,8 @@ def main():
     outfile_name = "conducting_sphere_potential_fieldsWithoutParticles.h5"
     extract_sphere_properties_from_out_file( outfile_name )
     node_x, node_y, node_z, phi = extract_full_nodecoords_and_potential_from_out_file( outfile_name )
-    num_subset = num_subset_along_z( node_x, node_y, node_z, phi )
-    an = an_potential_at_num_points( num_subset )
-    plot_comparision( num_subset, an )
+    plot_potential_in_plane( node_x, node_y, node_z, phi )
+    plot_potential_along_z( node_x, node_y, node_z, phi )
 
     
 def extract_sphere_properties_from_out_file( outfile_name ):
@@ -43,6 +42,30 @@ def extract_full_nodecoords_and_potential_from_out_file( outfile_name ):
     return( node_coords_x, node_coords_y, node_coords_z, num_potential )
 
 
+def plot_potential_in_plane( node_x, node_y, node_z, phi ):
+    num_subset = num_subset_in_middle_plane( node_x, node_y, node_z, phi )
+    an = an_potential_at_num_points( num_subset )
+    plot_num_potential_on_plane( num_subset, an )
+
+    
+def plot_potential_along_z( node_x, node_y, node_z, phi ):
+    num_subset = num_subset_along_z( node_x, node_y, node_z, phi )
+    an = an_potential_at_num_points( num_subset )
+    plot_comparision( num_subset, an )
+
+
+def num_subset_in_middle_plane( node_x, node_y, node_z, phi ):    
+    num_subset = []
+    for ( x, y, z, p ) in zip( node_x, node_y, node_z, phi ):
+        if ( y == sphere_center_y ):
+            num_subset.append( (x, y, z, p) )            
+    num_subset = np.array( num_subset,
+                           dtype=[('x', 'float'), ('y', 'float'),
+                                  ('z', 'float'), ('phi', 'float')] )
+    return( num_subset )
+
+
+    
 def num_subset_along_z( node_x, node_y, node_z, phi ):
     num_subset = []
     for ( x, y, z, p ) in zip( node_x, node_y, node_z, phi ):
@@ -77,7 +100,41 @@ def spherical_potential( x, y, z,
     return( potential )
 
 
-def plot_comparision( num, an ):    
+def plot_num_potential_on_plane( num, an ):
+    # todo: sort num before reshaping
+    # todo: get size from out-file
+    # num
+    plt.figure()
+    plt.subplot(121)
+    X = num['x'].reshape( ( 101,101 ) ) # 41 node with varying X and fixed Z, then next X-row with different Z. 
+    Y = num['z'].reshape( ( 101,101 ) )
+    Z = num['phi'].reshape( ( 101,101 ) )
+    levels = np.linspace( Z.min(), Z.max(), 12 )
+    CS = plt.contourf( X, Y, Z, levels )
+    cbar = plt.colorbar(CS)
+    cbar.ax.set_ylabel('Potential, [CGS]')
+    plt.xlabel( "X [cm]" )
+    plt.ylabel( "Z [cm]" )
+    plt.title('Num. potential at Y = y_size / 2')
+    # an
+    plt.subplot(122)
+    X = an['x'].reshape( ( 101,101 ) ) # 41 node with varying X and fixed Z, then next X-row with different Z. 
+    Y = an['z'].reshape( ( 101,101 ) )
+    Z = an['phi'].reshape( ( 101,101 ) )
+    levels = np.linspace( Z.min(), Z.max(), 12 )
+    CS = plt.contourf( X, Y, Z, levels )
+    cbar = plt.colorbar(CS)
+    cbar.ax.set_ylabel('Potential, [CGS]')
+    plt.xlabel( "X [cm]" )
+    plt.ylabel( "Z [cm]" )
+    plt.title('An. potential at Y = y_size / 2')
+    #plt.show()    
+    print( "Saving potential comparison to 'potential_in_plane.png'")
+    plt.savefig('potential_in_plane.png')
+
+
+def plot_comparision( num, an ):
+    plt.figure()
     plt.plot( num['z'], num['phi'], 
               linestyle='', marker='o',
               label = "Num" )
