a90a8f09c0fe8b16e96886456341c88ac2e9f130
noway <noway@011a-debian.jinr.ru>
2016-07-06 10:34:36 +0300

    Minor changes in diode Child's law example

--- examples/diode_childs_law/gen_configs_with_diff_voltages.py
+++ examples/diode_childs_law/gen_configs_with_diff_voltages.py
@@ -8,15 +8,15 @@ with open( basic_conf_file, 'r') as f:
 anode_voltage_linenum = 43
 output_file_name_linenum = 72
 
-voltages = list( range(10, 320, 30) )
+voltages = list( range(10, 1511, 50) )
 volts_to_cgs = 1.0 / 300.0
 
 for V in voltages:
-    conf_name = "V" + "{0:0=3d}".format(V) + ".conf"
+    conf_name = "V" + "{0:0=4d}".format(V) + ".conf"
     anode_voltage_line = "potential = +" + \
                          "{0:.5f}".format(V * volts_to_cgs ) + "\n"
     output_filename_prefix_line = "output_filename_prefix = V" + \
-                                  "{0:0=3d}".format(V) + "_" + "\n"
+                                  "{0:0=4d}".format(V) + "_" + "\n"
     basic_conf[anode_voltage_linenum] = anode_voltage_line
     basic_conf[output_file_name_linenum] = output_filename_prefix_line
     with open( conf_name, 'w') as f:
