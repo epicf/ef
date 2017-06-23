c1310ab5f5e11b6d10b9e1e4c9a83afb71290852
dumbman <dumbman@users.noreply.github.com>
2017-06-23 10:45:06 +0300

    Update contour_beam.py

--- examples/ribbon_beam_contour/contour_beam.py
+++ examples/ribbon_beam_contour/contour_beam.py
@@ -77,7 +77,7 @@ start_z, end_z = get_zlim( h5 )
 voltage = get_voltage( momentum_z, mass, charge )
 current_dens = get_current_dens(current,length_of_cathode)
 
-conv_grad_to_rad = np.pi/180
+conv_deg_to_rad = np.pi/180
 angle = 0 * conv_grad_to_rad          #angle of beam
 steps_z = 100
 position_z = np.arange(start_z,end_z,(end_z-start_z)/steps_z)                  # points in z direction, from 0 to 0.01 m with step 0,00001 m 
