9dfe30537c687d2a0516a52a3dda05192515e01b
noooway <a.a.letterbox@gmail.com>
2018-12-19 00:15:30 +0300

    In FieldSolver ComputePhiNext: neibhour -> neighbour

--- FieldSolver.cu
+++ FieldSolver.cu
@@ -41,25 +41,25 @@ __global__ void ComputePhiNext(const double* d_phi_current, const double* d_char
 	int offset_Dy = d_n_nodes[0].x;
 	int offset_Dz = d_n_nodes[0].x*d_n_nodes[0].y;
 
-	int prev_neibhour_idx;
-	int next_neibhour_idx;
+	int prev_neighbour_idx;
+	int next_neighbour_idx;
 
 	double denom = (double)2 * (dev_dxdxdydy[0] + dev_dxdxdzdz[0] + dev_dydydzdz[0]);
 
-	prev_neibhour_idx = max(idx + offset_Dx, 0);
-	next_neibhour_idx = min(idx + offset_Dx, dev_end[0]);
+	prev_neighbour_idx = max(idx + offset_Dx, 0);
+	next_neighbour_idx = min(idx + offset_Dx, dev_end[0]);
 	d_phi_next[idx] =
-		(d_phi_current[next_neibhour_idx] + d_phi_current[prev_neibhour_idx])*dev_dydydzdz[0];
+		(d_phi_current[next_neighbour_idx] + d_phi_current[prev_neighbour_idx])*dev_dydydzdz[0];
 
-	prev_neibhour_idx = max(idx + offset_Dy, 0);
-	next_neibhour_idx = min(idx + offset_Dy, dev_end[0]);
+	prev_neighbour_idx = max(idx + offset_Dy, 0);
+	next_neighbour_idx = min(idx + offset_Dy, dev_end[0]);
 	d_phi_next[idx] +=
-		(d_phi_current[next_neibhour_idx] + d_phi_current[prev_neibhour_idx])*dev_dxdxdzdz[0];
+		(d_phi_current[next_neighbour_idx] + d_phi_current[prev_neighbour_idx])*dev_dxdxdzdz[0];
 
-	prev_neibhour_idx = max(idx + offset_Dz, 0);
-	next_neibhour_idx = min(idx + offset_Dz, dev_end[0]);
+	prev_neighbour_idx = max(idx + offset_Dz, 0);
+	next_neighbour_idx = min(idx + offset_Dz, dev_end[0]);
 	d_phi_next[idx] +=
-		(d_phi_current[next_neibhour_idx] + d_phi_current[prev_neibhour_idx])*dev_dxdxdydy[0];
+		(d_phi_current[next_neighbour_idx] + d_phi_current[prev_neighbour_idx])*dev_dxdxdydy[0];
 
 	d_phi_next[idx] += 4.0 * CUDART_PI * d_charge[idx] * dev_dxdxdydydzdz[0];
 	d_phi_next[idx] /= denom;
