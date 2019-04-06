d14b4273976ba1579f05b4faae775e6dd0e4c561
andrew <andrew@dell.domainname>
2019-04-06 17:33:27 +0300

    SOR method for Poisson eqn

--- field_solver.h
+++ field_solver.h
@@ -18,22 +18,23 @@ class Field_solver {
     int nx, ny, nz;
     double dx, dy, dz;
   private:
-    int max_Jacobi_iterations;
+    int max_iterations;
     double rel_tolerance;
     double abs_tolerance;
     boost::multi_array<double, 3> phi_current;
     boost::multi_array<double, 3> phi_next;
     void allocate_current_next_phi();
     // Solve potential
-    void solve_poisson_eqn_Jacobi( Spatial_mesh &spat_mesh,
-				   Inner_regions_manager &inner_regions );
+    void solve_poisson_eqn_iterative( Spatial_mesh &spat_mesh,
+                                      Inner_regions_manager &inner_regions );
     void init_current_phi_from_spat_mesh_phi( Spatial_mesh &spat_mesh );
-    void single_Jacobi_iteration( Spatial_mesh &spat_mesh,
-				  Inner_regions_manager &inner_regions );
+    void single_solver_iteration( Spatial_mesh &spat_mesh,
+                                  Inner_regions_manager &inner_regions );
     void set_phi_next_at_boundaries();
-    void compute_phi_next_at_inner_points( Spatial_mesh &spat_mesh );
+    void compute_phi_next_at_inner_points_Jacobi( Spatial_mesh &spat_mesh );
+    void compute_phi_next_at_inner_points_SOR( Spatial_mesh &spat_mesh );
     void set_phi_next_at_inner_regions( Inner_regions_manager &inner_regions );
-    bool iterative_Jacobi_solutions_converged();
+    bool iterative_solution_converged( Spatial_mesh &spat_mesh );
     void set_phi_next_as_phi_current();
     void transfer_solution_to_spat_mesh( Spatial_mesh &spat_mesh );
     // Eval fields from potential
